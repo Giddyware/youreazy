876e1734236593cb0f70263578ad4f0be2bc21a5
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-07-12 10:06:29 +0100

    Worked on Navbar and Widget component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import "./home.scss";
+
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+      </div>
     </div>
   );
 };
