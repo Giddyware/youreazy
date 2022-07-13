fa39b28055b45510235a55f4430ca058ee2946ad
Giddy <Idowugidiyanuoluwa@gmail.com>
2022-07-13 18:43:55 +0100

    Added formSource.js in source directory and finishe the New page both for users and products

---
+++ src/formSource.js
@@ -0,0 +1,77 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "John Doe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+234 2345 678 90",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Willson St. 234 Green Rord",
+  },
+  {
+    id: 7,
+    label: "Username",
+    type: "text",
+    placeholder: "Canada",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Tyre",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Service",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,85 @@
-import './new.scss'
+import "./new.scss";
 
-const New = () => {
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
+  console.log(file);
+
   return (
-    <div>New</div>
-  )
-}
-export default New
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="title">{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form action="">
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              {/* <div className="formInput">
+                <label>Name and surname</label>
+                <input type="text" placeholder="Ali baba" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="text" placeholder="alibaba@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+234 567 89 00" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="text" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder=" Willson St. 234 Green Road" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="Canada" />
+              </div> */}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default New;
